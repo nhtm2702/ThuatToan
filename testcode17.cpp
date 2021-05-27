5d4e28171d8e885ea686829f3d23eba4afef97e5
nhtm2702 <78303807+nhtm2702@users.noreply.github.com>
2021-05-27 23:24:59 +0700

    updated

--- testcode17.cpp
+++ testcode17.cpp
@@ -3,16 +3,29 @@
 
 using namespace std;
 
+int find(int k, int *a, int start,int end)
+{
+    while (start <= end)
+    {
+        int mid = start+(end-start)/2; //tranh tran so
+        if (a[mid] < k) start = mid+1; //re phai
+        else if (a[mid] > k) end = mid -1; //re trai
+        else return mid;
+    }
+    return -1;
+}
+
 int main()
 {
     int n;
-    int *a = new int [n];
+    int a[10000];
     cin >> n;
     for (int i=0;i<n;i++) cin >> a[i];
     sort(a,a+n);
-    for (int i=0;i<n-2;i++)
-        for (int j=i+1;j<n-1;j++)
-            for (int k=j+1;k<n;k++)
-        if (a[i]+a[j]+a[k] == 0) cout << a[i] << " " << a[j] << " " << a[k] << endl;
+    for (int i=0;i<n;i++)
+    {
+        int j= find(-a[i],a,i+1,n-1);
+        if (j>0) cout << a[i] << " " << a[j] << endl;
+    }
     return 0;
 }
